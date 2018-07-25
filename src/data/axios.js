0a6e520d672bda3ea6dba5560a0ddd4b1b2d4b69
Lewis_A <lewisalderman95@gmail.com>
2018-07-25 12:38:21 +0100

    Increased page-size (number of articles)

--- src/data/axios.js
+++ src/data/axios.js
@@ -6,6 +6,7 @@ export default axios.create({
 		apiKey: "4dfac3162dd84ba8afaf01afd7e5c0d4",
 		language: "en",
 		sources: "techcrunch,techradar,the-verge,wired",
+		pageSize: "30",
 	},
 	headers: {
 		Accept: "application/json",
