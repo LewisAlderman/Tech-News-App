e07383213c0b2cebd70c1f9907c0c75e61ce95e3
Lewis_A <lewisalderman95@gmail.com>
2018-07-25 18:46:42 +0100

    Force article to load scrolled to top

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,30 +2,36 @@ import React from "react";
 import { connect } from "react-redux";
 import moment from "moment";
 // component
-const Article = ({ article, history }) => {
-	let published = date => moment(date).fromNow();
+class Article extends React.Component {
+	componentDidMount() {
+		window.scrollTo(0, 0);
+	}
+	render() {
+		let { article } = this.props;
+		let published = date => moment(date).fromNow();
 
-	return (
-		<article className="animated fadeIn" id="article">
-			<div className="img-container">
-				<img src={article.urlToImage} alt="source imagery" />
-			</div>
-			<h1 className="article-title">{article.title}</h1>
-			<p className="article-body">{article.description}</p>
-			<a href={article.url} target="_blank" rel="noopener noreferrer">
-				Read full story
-			</a>
-			<footer>
-				<p>
-					<span className="author">{article.author}</span>{" "}
-					<span className="seperator" /> {article.source.name}{" "}
-					<span className="seperator" />
-					{published(article.publishedAt)}
-				</p>
-			</footer>
-		</article>
-	);
-};
+		return (
+			<article className="animated fadeIn" id="article">
+				<div className="img-container">
+					<img src={article.urlToImage} alt="source imagery" />
+				</div>
+				<h1 className="article-title">{article.title}</h1>
+				<p className="article-body">{article.description}</p>
+				<a href={article.url} target="_blank" rel="noopener noreferrer">
+					Read full story
+				</a>
+				<footer>
+					<p>
+						<span className="author">{article.author}</span>{" "}
+						<span className="seperator" /> {article.source.name}{" "}
+						<span className="seperator" />
+						{published(article.publishedAt)}
+					</p>
+				</footer>
+			</article>
+		);
+	}
+}
 
 // container
 const mapStateToProps = (state, { id }) => {
